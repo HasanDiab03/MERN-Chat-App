a274da5dce5661dcdf18bd17447813894d99eab6
Hasan Diab <hasandiab.main@gmail.com>
2023-08-27 15:14:58 +0300

    endpoint

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -21,7 +21,7 @@ import io from "socket.io-client";
 import animationData from "../animations/typing.json";
 import Lottie from "react-lottie";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://chatty-yijl.onrender.com";
 let socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
