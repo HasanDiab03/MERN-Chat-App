fb28c7123ac4a42b41e6e0db9267d1fd7ae17e38
Hasan Diab <hasandiab.main@gmail.com>
2023-09-10 16:00:10 +0300

    redeploy using netlify for frontend

--- backend/index.js
+++ backend/index.js
@@ -21,17 +21,17 @@ app.use("/api/notification", notificationRoutes);
 
 // ---------------- Deployment -----------------
 
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running Successfully");
-  });
-}
+// const __dirname1 = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running Successfully");
+//   });
+// }
 
 // ---------------- Deployment ----------------
 app.use(notFound);
